724c13ec1722e4d23e7fb03aac10f579d02cd9b7
shiming-shimu <62317052+shiming-shimu@users.noreply.github.com>
2020-12-06 10:44:22 +0800

    test

--- js/card.js
+++ js/card.js
@@ -1,4 +1,4 @@
-$(document).ready(function () {
+function setCard() {
     // move 
     $(".card .card-top").mousedown(function (event) { 
         var cx = event.pageX - $(this).parent().position().left
@@ -11,4 +11,19 @@ $(document).ready(function () {
             $(".card .card-top").off("mousemove")
         })
     })
-})
+    $(".card .card-top .card-circle-yellow").click(function () { 
+        $(this).parent().parent().hide();
+        var info = $.parseJSON($(this).parent().parent().attr("data-info"));
+        $(".passages").html($(".passages").html()+'\
+        <div class="item"  data-info=\'{"title": "'+ info["title"] +'", "user": "'+ info["user"] +'", "date": "'+ info["date"] +'", "preview": "'+ info["preview"] +'"}\'>\
+            <h2>title</h2>\
+            <div class="info">\
+                <span class="user">shiming</span>\
+                <span class="date">2020.12.6</span>\
+                <p class="preview">你好</p>\
+            </div>\
+        </div>\
+        ')
+        addCard()
+     })
+}
