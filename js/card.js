13b53e918eddce429f6b574769c3f11fbca18c16
shiming-shimu <62317052+shiming-shimu@users.noreply.github.com>
2020-12-11 21:51:54 +0800

    必拿下

--- js/card.js
+++ js/card.js
@@ -1,5 +1,33 @@
-function setCard() {
-    // move 
+//读取 添加卡片
+function addCard(){
+    $.getJSON("passages/list.json",
+        function(data){data.forEach(info => {
+            $(".passages").html($(".passages").html()+'\
+                <div class="item" id="'+info["path"]+'" data-info=\'{"title": "'+ info["title"] +'", "user": "'+ info["user"] +'", "date": "'+ info["date"] +'", "preview": "'+ info["des"] +'", "path": "'+ info["path"] +'"}\'>\
+                    <h2>'+ info["title"] +'</h2>\
+                    <div class="info">\
+                        <span class="user">'+ info["user"] +'</span>\
+                        <span class="date">'+ info["date"] +'</span>\
+                        <p class="preview">'+ info["des"] +'</p>\
+                    </div>\
+                </div>');
+            $(".cards").html($(".cards").html() + ' \
+                <div class="card" id="card-'+info["path"]+'" style="display: none;width: 350px; top: 100px;" data-info=\'{"title": "'+ info["title"] +'", "user": "'+ info["user"] +'", "date": "'+ info["date"] +'", "preview": "'+ info["des"] +'", "path": "'+ info["path"] +'"}\'> \
+                <div class="card-top"> \
+                    <div class="card-circle-red"></div> \
+                    <div class="card-circle-yellow"></div> \
+                    <div class="card-circle-green"></div> \
+                    <div class="card-title"> '+ info["title"] +' </div> \
+                </div> \
+                <div class="card-body">'+info["des"]+'</div>');
+            }
+        )}
+    );
+}
+
+//设置卡片事件
+function setCardEvent() {
+    // 移动事件
     $(".card .card-top").mousedown(function (event) { 
         var cx = event.pageX - $(this).parent().position().left
         var cy = event.pageY - $(this).parent().position().top
@@ -11,12 +39,26 @@ function setCard() {
             $(".card .card-top").off("mousemove")
         })
     })
+    //黄色最小化按钮
     $(".card .card-top .card-circle-yellow").click(function () { 
         $(this).parent().parent().hide();
-        var info = $.parseJSON($(this).parent().parent().attr("data-info"));
-        addCard()
+     })
+     //绿色最大化按钮
+     $(".card .card-top .card-circle-green").click(function () { 
+        $(this).parent().parent().hide();
+        
      })
 }
+
+//设置文章事件
+function setPassageEvent() {
+    $(".passages .item").click(function(){
+        $( ($.parseJSON($(this).attr("data-info")))["id"] ).hide();
+        $("id-" + ($.parseJSON($(this).attr("data-info")))["id"] ).show();
+    })
+}
+
+
 
 // $(".passages").html($(".passages").html()+'\
 // <div class="item"  data-info=\'{"title": "'+ info["title"] +'", "user": "'+ info["user"] +'", "date": "'+ info["date"] +'", "preview": "'+ info["preview"] +'"}\'>\
